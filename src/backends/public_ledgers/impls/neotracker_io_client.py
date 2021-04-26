984a256b2c55c134ba1cad47c180cf8098414a75
financelurker <financelurker_github@blunzn.club>
2021-04-26 08:52:11 +0200

    added Neo Blockchain Module

--- src/backends/public_ledgers/impls/neotracker_io_client.py
+++ src/backends/public_ledgers/impls/neotracker_io_client.py
@@ -1,33 +1,72 @@
+from typing import List
+
 from syncer import sync
 import aiohttp
 
+from backends.public_ledgers import SupportedBlockchainModule, SupportedBlockchainExplorer
 from model.ledger_transaction import LedgerTransaction
 
+
+# Module config
+name = "Neo"
+currency_code = "NEO"
+address_identifier = "address"
+address_regular_expression = r"address=\"([a-zA-Z0-9]*)\""
+
+# Backend config
 base_url = "https://neoscan.io"
 last_transactions_uri = "/api/main_net/v1/get_last_transactions_by_address/{address}/{page}"
 get_transaction_uri = "/api/main_net/v1/get_transaction/"
 
 
-@sync
-async def get_transaction_from_ledger(tx_id, timeout=25) -> LedgerTransaction:
-    timeout = aiohttp.ClientTimeout(timeout)
-    async with aiohttp.ClientSession(timeout=timeout) as session:
-        resp = await session.request(method="get", url=base_url + get_transaction_uri + tx_id)
-        resp_json = await resp.json()
-        return LedgerTransaction(
-            txId=resp_json.get("txid"),
-            ins=[
-                in_address.get("address_hash")
-                for in_address in resp_json.get("vin")
-            ],
-            outs=[
-                in_address.get("address_hash")
-                for in_address in resp_json.get("vouts")
-            ]
-        )
+@SupportedBlockchainModule.register
+class NeoExplorerModule(SupportedBlockchainModule):
+
+    def get_blockchain_explorer(self) -> SupportedBlockchainExplorer:
+        return NeoExplorer()
+
+    def get_blockchain_name(self) -> str:
+        return "NEO"
+
+    def is_enabled(self) -> bool:
+        return True
+
+    @staticmethod
+    def get_instance(cls):
+        return NeoExplorerModule()
 
 
-@sync
-async def get_addresses_from_xpub(x_pub: str, timeout=25):
-    addresses = [x_pub]
-    return addresses
+@SupportedBlockchainExplorer.register
+class NeoExplorer(SupportedBlockchainExplorer):
+
+    def get_tx_addresses_from_address(self, address: str, timeout=25) -> List[str]:
+        return [address]
+
+    def get_blockchain_name(self) -> str:
+        return name
+
+    def get_currency_code(self) -> str:
+        return currency_code
+
+    def get_address_identifier(self) -> str:
+        return address_identifier
+
+    def get_address_re(self) -> str:
+        return address_regular_expression
+
+    def get_transaction_from_ledger(self, tx_id, timeout=25) -> LedgerTransaction:
+        timeout = aiohttp.ClientTimeout(timeout)
+        async with aiohttp.ClientSession(timeout=timeout) as session:
+            resp = await session.request(method="get", url=base_url + get_transaction_uri + tx_id)
+            resp_json = await resp.json()
+            return LedgerTransaction(
+                txId=resp_json.get("txid"),
+                ins=[
+                    in_address.get("address_hash")
+                    for in_address in resp_json.get("vin")
+                ],
+                outs=[
+                    in_address.get("address_hash")
+                    for in_address in resp_json.get("vouts")
+                ]
+            )
